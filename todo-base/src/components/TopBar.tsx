704f6a5e0559a8214c36382cd97098eafa97ae07
Jack Herrington <jherr@pobox.com>
2021-02-08 10:45:44 -0800

    Updates

--- todo-base/src/components/TopBar.tsx
+++ todo-base/src/components/TopBar.tsx
@@ -2,6 +2,10 @@ import * as React from "react";
 import { Button, Grid } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
+/*
+JSON source: https://raw.githubusercontent.com/jherr/todos-four-ways/master/data/todos.json
+*/
+
 function TopBar() {
   return (
     <Grid pt={2} templateColumns="1fr 1fr" columnGap="3">
